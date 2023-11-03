d98c786181fc2a8d5993605075d947b2579e4606
Voky <voky.consultant@gmail.com>
2023-11-03 21:03:43 +0100

    Update hw01.py

--- hw01.py
+++ hw01.py
@@ -1,6 +1,6 @@
 # https://cw.fel.cvut.cz/wiki/courses/bab37zpr/hw/hw01
 
-def triangle(inc:bool, width:int, rows:int, gap:int):
+def triangle(inc:bool, width:int, rows:int, gap:int, symbol:str):
     start:int
     end:int
     step:int
@@ -18,24 +18,56 @@ def triangle(inc:bool, width:int, rows:int, gap:int):
         for j in range(int((width-i)/2)):
             print(" ", end="")
         for j in range(i):
-            print("x", end="")
+            print(symbol, end="")
         print("")
 
-def sand_hours(width:int):
+def row(border:str, inside:str, border_width:int, inside_width:int):
+    for i in range(border_width):
+        print(border, end="")
+    for i in range(inside_width):
+        print(inside, end="")
+    for i in range(border_width):
+        print(border, end="")
+    print("")
+
+def funkce_1(width:int) -> int:
     if(width % 2 == 0):
-        raise Exception("sand_hours only except odd numbers")
-    print("$", end="")
-    for i in range(width-2):
-        print("-", end="")
-    print("$")
-    triangle(False, width-2, int((width-2 + 1) / 2), 1)
-    triangle(True,  width-2, int((width-2 + 1) / 2)-1, 1)
-    print("$", end="")
-    for i in range(width-2):
-        print("-", end="")
-    print("$")
+        raise Exception("funkce_1 only except odd numbers")
+    if(width < 3 or width >= 20):
+        return 1
+    row("$","-",1,width-2)
+    triangle(False, width-2, int((width-2 + 1) / 2), 1, "x")
+    triangle(True,  width-2, int((width-2 + 1) / 2)-1, 1, "x")
+    row("$","-",1,width-2)
+    return 0
 
+def funkce_2(width:int, height:int):
+    if(width % 2 == 0):
+        raise Exception("funkce_2 only except odd numbers")
+    if(width < 3 or width >= 20):
+        return 1
+    if(height >= width):
+        return 2
+    print(" "* int(width/2)+"o")
+    triangle(True,width,int(width/2),0,"@")
+    for i in range(height):
+        row("@","x",1,width-2)
+    triangle(False,width,int(width/2),0,"@")
+    print(" "* int(width/2)+"o")
+    return 0
 
+def funkce_3(width:int, height:int, char:str):
+    if(width % 2 == 0):
+        raise Exception("funkce_2 only except odd numbers")
+    if(width < 3 or width >= 20):
+        return 1
+    if(height >= width):
+        return 2
+    if(char < "a" or char > "z"):
+        return 3
+    triangle(True,width,int((width+1)/2),0,"^")
+    for i in range(height):
+        row("|", char, 1, width-2)
+    return 0
 
-
-sand_hours(11)
+funkce_3(9, 3, "a")
