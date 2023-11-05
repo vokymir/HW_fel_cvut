3b4f13a445a88e30f470fc3e270c03dc86f07fb1
Voky <voky.consultant@gmail.com>
2023-11-05 20:20:15 +0100

    Update hw03.py
    Trying to achieve working program.
    It returns wrong value, but at last, it's returning something.

--- hw03.py
+++ hw03.py
@@ -1,21 +1,45 @@
 # https://cw.fel.cvut.cz/wiki/courses/bab37zpr/hw/hw03
 
+class Guess:
+    def __init__(self, shiftBy:int, shiftedTo:str, similarTo:int):
+        self.shiftBy = shiftBy
+        self.shiftedTo = shiftedTo
+        self.similarTo = similarTo
+
+
 def dekoduj(sifrovany:str, odposlechnuty:str):
-    abc:list[str] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    for x in sifrovany:
-        if(x>"Z" or x<"a" or (x>"z" and x<"A")):
+    #abc:list[str] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    abc:str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(len(sifrovany)):
+        x = sifrovany[i]
+        if(x>"z" or x<"A" or (x>"Z" and x<"a")):
             print("Error: Chybny vstup!")
             return None
-    for x in odposlechnuty:
-        if(x>"Z" or x<"a" or (x>"z" and x<"A")):
+    for i in range(len(odposlechnuty)):
+        x = odposlechnuty[i]
+        if(x>"z" or x<"A" or (x>"Z" and x<"a")):
             print("Error: Chybny vstup!")
             return None
     if(len(sifrovany) != len(odposlechnuty)):
         print("Error: Chybna delka vstupu!")
         return None
-    
+    resemblence:list[Guess] = []
+    for i in range(len(abc)):
+        shifted:str = ""
+        similarity:int = 0
+        for j in range(len(sifrovany)):
+            y:int = (int(abc.index(sifrovany[j]))+i)%len(abc)
+            shifted = shifted + abc[y]
+            if(shifted[j]==odposlechnuty[j]):
+                similarity += 1
+        resemblence.append(Guess(i,shifted,similarity))
+    res:str = sifrovany
+    howmuch:int = 0
+    for i in range(len(resemblence)):
+        if(resemblence[i].similarTo > howmuch):
+            res = resemblence[i].shiftedTo
+
+    return res
 
 
-
-
-    return "ahoj"
+print(dekoduj("xUbbemehbT","XYlloworld"))
