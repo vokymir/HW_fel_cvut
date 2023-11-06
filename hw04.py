c5cb39dd109ca479fb8108ea2d3202c3f968ff26
Voky <voky.consultant@gmail.com>
2023-11-06 14:52:43 +0100

    Update hw04.py
    excluding non-letter characters

--- hw04.py
+++ hw04.py
@@ -8,15 +8,18 @@ class Symbol:
 def histogram(text:str, scale:int=1, case_sensitive:bool=False):
     Symbols:list[Symbol] = []
     for x in text:
-        done:bool = False
-        for y in Symbols:
-            if(x == y.symbol):
-                y.count += 1
-                done = True
-                break
-        if (done == False):
-                Symbols.append(Symbol(x))
+        if (not(x<"A" or x>"z" or (x>"Z" and x <"a"))):
+            done:bool = False
+            for y in Symbols:
+            
+                if(x == y.symbol):
+                    y.count += 1
+                    done = True
+                    break
+            if (done == False):
+                    Symbols.append(Symbol(x))
 
+        
     for i in range(len(Symbols)):
         for j in range(len(Symbols)-i-1):
             if(Symbols[j].symbol > Symbols[j+1].symbol):
@@ -32,4 +35,4 @@ def histogram(text:str, scale:int=1, case_sensitive:bool=False):
     
     return SymbolsII
 
-print(histogram("wabcdabcdsgjthwoievufnwohnaciugfqoaerucgynqpaoidcfhaewhiofaba"))
+histogram("ah jo . js")
