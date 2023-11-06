e242e72b6c48c44293835477a13fbf6c18066373
Voky <voky.consultant@gmail.com>
2023-11-06 13:54:52 +0100

    Update hw04.py
    it counts how many times I have a char in the whole string

--- hw04.py
+++ hw04.py
@@ -1,16 +1,24 @@
 # https://cw.fel.cvut.cz/wiki/courses/bab37zpr/hw/hw04
 
 class Symbol:
-    def __init__(self, symbol:str, count:int=0) -> None:
+    def __init__(self, symbol:str, count:int=1):
         self.symbol = symbol
         self.count = count
 
-def histogram(text:str, scale:int=0, case_sensitive:bool=False):
+def histogram(text:str, scale:int=1, case_sensitive:bool=False):
     Symbols:list[Symbol] = []
     for x in text:
-        Symbols.append(Symbol(x))
+        done:bool = False
+        for y in Symbols:
+            if(x == y.symbol):
+                y.count += 1
+                done = True
+                break
+        if (done == False):
+                Symbols.append(Symbol(x))
+
     for i in Symbols:
-        print(i.symbol, end="")
+        print(i.symbol, end=" ")
         print(i.count)
 
 histogram("abcdabcaba")
