5bb43025a5efcbf1383b202cd0a0937d3281306f
Voky <voky.consultant@gmail.com>
2023-11-06 20:26:22 +0100

    Update hw04.py
    All done, except for cleaning and optimization.

--- hw04.py
+++ hw04.py
@@ -22,7 +22,6 @@ def histogram(text:str, scale:int=1, case_sensitive:bool=False) -> list:
         if (not(x<"A" or x>"z" or (x>"Z" and x <"a"))):
             done:bool = False
             for y in Symbols:
-            
                 if(x == y.symbol):
                     y.count += 1
                     if y.count > biggest:
@@ -56,51 +55,70 @@ def histogram(text:str, scale:int=1, case_sensitive:bool=False) -> list:
 def serad(text:str, metoda:int, case_sensitive:bool = False) -> list:
     res:list[tuple] = []
     Htext:str = ""
+    # get rid of non-alphabetical characters
     for i in text:
         if (not(i<"A" or i>"z" or (i>"Z" and i <"a")) or i==" "):
             Htext = Htext + i
     text = Htext
 
-    if not(case_sensitive):
-        text = text.casefold()
     
     words:list[str] = text.split(" ")
     
     for i in words:
+        #get rid of short words
         if len(i)<4:
             words.remove(i)
         else:
+            # compare by word length
             if metoda == 0:
                 res.append((i, len(i)))
+            # compare by consonant (not-vowel) -> subtract count of vowel
             elif metoda == 1:
                 res.append((i, len(i) - i.count("a") - i.count("A") - i.count("e") -i.count( "E") -i.count( "i") -i.count( "I") - i.count( "y") -i.count("Y") -i.count("o")-i.count( "O") -i.count("u") -i.count("U")))
-            elif metoda == 3:
-                pass
-
-    for j in range(len(res)-1):
+            # compare by most-used char
+            elif metoda == 2:
+                Symbols:list[Symbol] = []
+                biggest:int = 0
+                # for every letter in each word
+                for pismeno in i:
+                    done:bool = False
+                    # case sensitivness done
+                    if case_sensitive:
+                        pismenko = pismeno
+                    else:
+                        pismenko = pismeno.casefold()
+                    # check, if already counting, if so add 1, otherwise start counting
+                    for znak in Symbols:
+                        if(pismenko == znak.symbol):
+                            znak.count += 1
+                            if znak.count > biggest:
+                                biggest = znak.count
+                            done = True
+                            break
+                    if (done == False):
+                        Symbols.append(Symbol(pismenko))
+                # append word with how many times the most used char was used
+                res.append((i, biggest))
+    # sort by numeric value (dependant on method)
+    for j in range(len(res)):
         for k in range(len(res)-j-1):
             if res[k][1] < res[k+1][1]:
                 help:tuple = res[k]
                 res[k] = res[k+1]
                 res[k+1] = help
-            
+    # sort by alphabet
+    for j in range(len(res)):
+        for k in range(len(res)-j-1):
+            if res[k][1] == res[k+1][1] and res[k][0] > res[k+1][0]:
+                help = res[k]
+                res[k] = res[k+1]
+                res[k+1] = help        
+    # copy res to resres in desired output
+    resres:list[str] = []
+    for l in range(len(res)):
+        resres.append(res[l][0])
 
-    return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return resres
 
 #tests from assignment
 """
@@ -109,4 +127,8 @@ histogram('Ahoj svete, kde to kvete', 10)
 histogram('Aaaach, to je kraaasa', 4)
 """
 
-print(serad('Aaaach, to je kraaasa papelelelelele kukukuku', 0, True))
+"""
+print(serad('Aaaach, to je kraaasa', 0, True))
+print(serad('Aaaach, to je kraaasa', 2, True))
+print(serad('Aaaach, to je kraaasa', 2, False))
+"""
