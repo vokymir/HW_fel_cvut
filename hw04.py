872400af0ecd1db2aaa85b4c0ad7697e66d4ca63
Voky <voky.consultant@gmail.com>
2023-11-06 13:00:50 +0100

    Update hw04.py
    trying how classes works

--- hw04.py
+++ hw04.py
@@ -1,1 +1,16 @@
+# https://cw.fel.cvut.cz/wiki/courses/bab37zpr/hw/hw04
 
+class Symbol:
+    def __init__(self, symbol:str, count:int=0) -> None:
+        self.symbol = symbol
+        self.count = count
+
+def histogram(text:str, scale:int=0, case_sensitive:bool=False):
+    Symbols:list[Symbol] = []
+    for x in text:
+        Symbols.append(Symbol(x))
+    for i in Symbols:
+        print(i.symbol, end="")
+        print(i.count)
+
+histogram("abcdabcaba")
